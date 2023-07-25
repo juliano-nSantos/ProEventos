7a3ed7c4558cdfe3b6a774855071973fc8746f2c
Juliano Nascimento <julianonascimento.ti@gmail.com>
2023-07-24 21:53:05 -0300

    Secao 3: EF Core 5 Introducao

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -11,43 +12,26 @@ namespace ProEventos.API.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
-    {     
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento {
-                EventoId = 1,
-                Tema = "Angular 11 e .Net 5",
-                Local = "Belo Horizonte",
-                Lote = "1° Lote",
-                Quantidade = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemUrl = "foto.png"
-            },
-             new Evento {
-                EventoId = 2,
-                Tema = "Angular 11 e suas novidades",
-                Local = "São Paulo",
-                Lote = "2° Lote",
-                Quantidade = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemUrl = "foto1.png"
-            }
-        };
+    {
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
-            
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-           return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(even => even.EventoId == id);
+            return _context.Eventos.FirstOrDefault(
+                even => even.EventoId == id
+            );
         }
     }
 }
